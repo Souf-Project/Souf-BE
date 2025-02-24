52ec8a639f31c0d851711a19f3220e6c2be5e803
wjd4204 <brian876373@gmail.com>
2025-02-25 02:07:35 +0900

    feat: add first commit

---
+++ src/main/java/com/souf/soufwebsite/SoufwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.souf.soufwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoufwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoufwebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/souf/soufwebsite/SoufwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souf.soufwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoufwebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
