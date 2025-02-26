e0e0ac20e23246626c85623b3eb5f1c9094020ac
tjdgus3488 <tjdgus3488@naver.com>
2025-02-27 01:24:09 +0900

    feat: Chat 엔티티 추가

--- src/main/java/com/souf/soufwebsite/domain/chat/entity/Chat.java
+++ src/main/java/com/souf/soufwebsite/domain/chat/entity/Chat.java
@@ -1,4 +1,26 @@
 package com.souf.soufwebsite.domain.chat.entity;
 
-public class Chat {
+import com.souf.soufwebsite.domain.user.entity.User;
+import com.souf.soufwebsite.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
 }
