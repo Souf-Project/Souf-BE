26d35a3f8ce414d505f1e66c8748c1694a287378
wjd4204 <brian876373@gmail.com>
2025-03-14 23:43:04 +0900

    feat: RecruitController 구현

--- src/main/java/com/souf/soufwebsite/domain/recruit/controller/RecruitController.java
+++ src/main/java/com/souf/soufwebsite/domain/recruit/controller/RecruitController.java
@@ -1,10 +1,14 @@
 package com.souf.soufwebsite.domain.recruit.controller;
 
+import com.souf.soufwebsite.domain.recruit.dto.RecruitReqDto;
+import com.souf.soufwebsite.domain.recruit.dto.RecruitResDto;
 import com.souf.soufwebsite.domain.recruit.service.RecruitService;
+import com.souf.soufwebsite.global.common.FirstCategory;
+import com.souf.soufwebsite.global.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +17,33 @@ public class RecruitController {
 
     private final RecruitService recruitService;
 
+    @PostMapping
+    public SuccessResponse<?> createRecruit(RecruitReqDto recruitReqDto) {
+        recruitService.createRecruit(recruitReqDto);
 
+        return new SuccessResponse<>("Recruit created successfully");
+    }
+
+    @GetMapping
+    public SuccessResponse<List<RecruitResDto>> getRecruits(@RequestParam(name = "category") FirstCategory category) {
+        return new SuccessResponse<>(recruitService.getRecruits(category));
+    }
+
+    @GetMapping("/{recruitId}")
+    public SuccessResponse<RecruitResDto> getRecruitById(@PathVariable(name = "recruitId") Long recruitId) {
+        return new SuccessResponse<>(recruitService.getRecruitById(recruitId));
+    }
+
+    @PatchMapping("/{recruitId}")
+    public SuccessResponse<?> updateRecruit(@PathVariable(name = "recruitId") Long recruitId, RecruitReqDto recruitReqDto) {
+        recruitService.updateRecruit(recruitId, recruitReqDto);
+
+        return new SuccessResponse<>("Recruit updated successfully");
+    }
+
+    @DeleteMapping("/{recruitId}")
+    public SuccessResponse<?> deleteRecruit(@PathVariable(name = "recruitId") Long recruitId) {
+        recruitService.deleteRecruit(recruitId);
+        return new SuccessResponse<>("Recruit deleted successfully");
+    }
 }
