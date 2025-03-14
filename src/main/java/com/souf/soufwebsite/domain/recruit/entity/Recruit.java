7ac297d6399f56c03623892ded3439f661a557ce
wjd4204 <brian876373@gmail.com>
2025-03-14 21:39:42 +0900

    refactor: Recruit 엔티티 클래스 빌더 패턴 수정 및 메서드 추가

--- src/main/java/com/souf/soufwebsite/domain/recruit/entity/Recruit.java
+++ src/main/java/com/souf/soufwebsite/domain/recruit/entity/Recruit.java
@@ -3,6 +3,7 @@ package com.souf.soufwebsite.domain.recruit.entity;
 import com.souf.soufwebsite.domain.recruit.dto.RecruitReqDto;
 import com.souf.soufwebsite.domain.user.entity.User;
 import com.souf.soufwebsite.global.common.BaseEntity;
+import com.souf.soufwebsite.global.common.FirstCategory;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -33,7 +34,18 @@ public class Recruit extends BaseEntity {
     private String region;
 
     // 마감일자
+    @Column
     private LocalDateTime deadline;
+
+    @Column(nullable = false)
+    private String payment;
+
+    @Column
+    private String preferentialTreatment;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FirstCategory firstCategory;
 
     // 작성자 (userId)
     @ManyToOne(fetch = FetchType.LAZY)
@@ -41,23 +53,33 @@ public class Recruit extends BaseEntity {
     private User user;
 
     @Builder
-    public Recruit(Long id, String title, String content, String region, LocalDateTime deadline, User user) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.region = region;
+    public Recruit(RecruitReqDto reqDto, LocalDateTime deadline, User user) {
+        this.title = reqDto.title();
+        this.content = reqDto.content();
+        this.region = reqDto.region();
         this.deadline = deadline;
+        this.payment = reqDto.payment();
+        this.preferentialTreatment = reqDto.preferentialTreatment();
+        this.firstCategory = reqDto.firstCategory();
         this.user = user;
     }
 
-    public static Recruit of(RecruitReqDto reqDto, User user) {
+    public static Recruit of(RecruitReqDto reqDto, LocalDateTime deadline, User user) {
         return Recruit.builder()
-                .title(reqDto.title())
-                .content(reqDto.content())
-                .region(reqDto.region())
-                .deadline(getDeadLine(reqDto.deadLine()))
+                .reqDto(reqDto)
+                .deadline(deadline)
                 .user(user)
                 .build();
+    }
+
+    public void updateRecruit(RecruitReqDto reqDto) {
+        this.title = reqDto.title();
+        this.content = reqDto.content();
+        this.region = reqDto.region();
+        this.deadline = getDeadLine(reqDto.deadline());
+        this.payment = reqDto.payment();
+        this.preferentialTreatment = reqDto.preferentialTreatment();
+        this.firstCategory = reqDto.firstCategory();
     }
 
     private static LocalDateTime getDeadLine(String deadLine){
