fb4c453d88c083b853e727e0a6f461d2ff015e13
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2025-05-13 18:29:56 +0900

    feat: 카테고리 테이블 추가

---
+++ src/main/java/com/souf/soufwebsite/global/common/category/FirstCategory.java
@@ -0,0 +1,26 @@
+package com.souf.soufwebsite.global.common.category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FirstCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "first_category_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "firstCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SecondCategory> secondCategoryList = new ArrayList<>();
+
+}
--- src/test/java/com/souf/soufwebsite/domain/recruit/service/RecruitServiceImplTest.java
+++ src/test/java/com/souf/soufwebsite/domain/recruit/service/RecruitServiceImplTest.java
@@ -1,25 +1,15 @@
 package com.souf.soufwebsite.domain.recruit.service;
 
 import com.souf.soufwebsite.domain.member.entity.Member;
-import com.souf.soufwebsite.domain.recruit.dto.RecruitReqDto;
 import com.souf.soufwebsite.domain.recruit.entity.Recruit;
 import com.souf.soufwebsite.domain.recruit.repository.RecruitRepository;
-import com.souf.soufwebsite.global.common.FirstCategory;
-import com.souf.soufwebsite.global.util.SecurityUtils;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.client.ExpectedCount.times;
-
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RecruitServiceImplTest {
@@ -44,8 +34,8 @@ class RecruitServiceImplTest {
 //    @DisplayName("공고문을 성공적으로 생성합니다.")
 //    @Test
 //    void createRecruit(){
-//        RecruitReqDto reqDto = new RecruitReqDto("title1", "content1", "서울시 도봉구", "2025/03/24 10:30",
-//                "100만원", "우대사항1", FirstCategory.MUSIC);
+//        RecruitReqDto reqDto = new RecruitReqDto("title1", "content1", "서울시 도봉구", LocalDateTime.,
+//                "100만원", "우대사항1", FirstCategory.MUSIC, List.of("file.jpg"));
 //
 //        given(recruitRepository.save(any(Recruit.class))).willAnswer(invocation -> invocation.getArgument(0));
 //
