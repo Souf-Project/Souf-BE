d29407fe5c718d25eb0be50f8c87c3c79ff49bf0
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2025-05-21 21:52:57 +0900

    feat: RedisUtil 추가

---
+++ src/main/java/com/souf/soufwebsite/global/redis/util/RedisUtil.java
@@ -0,0 +1,32 @@
+package com.souf.soufwebsite.global.redis.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final RedisTemplate<String, Long> redisTemplate;
+
+    public void set(String key) {
+        redisTemplate.opsForValue().set(key, 0L);
+    }
+
+    public Long get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public Set<String> getKeys(String key){
+        return redisTemplate.keys(key);
+    }
+
+    public void increaseCount(String key){
+        redisTemplate.opsForValue().increment(key);
+    }
+
+}
