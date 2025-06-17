441a1996a180a4771f6e40698b26398eb0b1b9a2
tjdgus3488 <tjdgus3488@naver.com>
2025-06-17 16:34:47 +0900

    fix: 인기 피드 반환값 수정

--- src/main/java/com/souf/soufwebsite/domain/feed/dto/FeedSimpleResDto.java
+++ src/main/java/com/souf/soufwebsite/domain/feed/dto/FeedSimpleResDto.java
@@ -4,12 +4,17 @@ import com.souf.soufwebsite.domain.feed.entity.Feed;
 import com.souf.soufwebsite.domain.file.dto.MediaResDto;
 
 public record FeedSimpleResDto(
-        Long feedId,
+        String nickname,
+        String categoryName,
         MediaResDto mediaResDto
 ) {
     public static FeedSimpleResDto from(Feed feed , MediaResDto mediaResDto) {
-        return new FeedSimpleResDto(feed.getId(),
-                mediaResDto
-        );
+        String nickname = feed.getMember().getNickname();
+        String categoryName = feed.getCategories().stream()
+                .findFirst()
+                .map(cat -> cat.getSecondCategory().getName())
+                .orElse(null);
+
+        return new FeedSimpleResDto(nickname, categoryName, mediaResDto);
     }
 }
