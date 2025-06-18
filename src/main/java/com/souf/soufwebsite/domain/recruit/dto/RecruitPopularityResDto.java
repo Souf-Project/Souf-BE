6433cbd213de5bcd0f5915ee7f222757c2beeff4
wjd4204 <brian876373@gmail.com>
2025-06-18 20:11:01 +0900

    fix: 반환 필드에 데드라인 추가

--- src/main/java/com/souf/soufwebsite/domain/recruit/dto/RecruitPopularityResDto.java
+++ src/main/java/com/souf/soufwebsite/domain/recruit/dto/RecruitPopularityResDto.java
@@ -2,12 +2,15 @@ package com.souf.soufwebsite.domain.recruit.dto;
 
 import com.souf.soufwebsite.domain.recruit.entity.Recruit;
 
+import java.time.LocalDateTime;
+
 public record RecruitPopularityResDto(
         Long recruitId,
         String title,
         String content,
         Long firstCategory,
-        Long secondCategory
+        Long secondCategory,
+        LocalDateTime deadLine
 ) {
     public static RecruitPopularityResDto of(Recruit recruit) {
         return new RecruitPopularityResDto(
@@ -15,7 +18,8 @@ public record RecruitPopularityResDto(
                 recruit.getTitle(),
                 recruit.getContent(),
                 recruit.getCategories().get(0).getFirstCategory().getId(),
-                recruit.getCategories().get(0).getSecondCategory().getId()
+                recruit.getCategories().get(0).getSecondCategory().getId(),
+                recruit.getDeadline()
         );
     }
 }
