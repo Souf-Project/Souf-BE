a9eaec5da6ec7d19dbdaef90eeda39b5a17b6cb7
wjd4204 <brian876373@gmail.com>
2025-06-26 15:23:47 +0900

    fix: 피드 상세 조회 시 카테고리 반환

--- src/main/java/com/souf/soufwebsite/domain/feed/dto/FeedDetailResDto.java
+++ src/main/java/com/souf/soufwebsite/domain/feed/dto/FeedDetailResDto.java
@@ -1,9 +1,11 @@
 package com.souf.soufwebsite.domain.feed.dto;
 
 import com.souf.soufwebsite.domain.feed.entity.Feed;
+import com.souf.soufwebsite.domain.feed.entity.FeedCategoryMapping;
 import com.souf.soufwebsite.domain.file.dto.MediaResDto;
 import com.souf.soufwebsite.domain.file.entity.Media;
 import com.souf.soufwebsite.domain.member.entity.Member;
+import com.souf.soufwebsite.global.common.category.dto.CategoryDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,6 +21,7 @@ public record FeedDetailResDto(
         String content,
         Long view,
         List<MediaResDto> mediaResDtos,
+        List<CategoryDto> categoryDtos,
         LocalDateTime lastModifiedTime
 ) {
     public static FeedDetailResDto from(Member member, String profileUrl, Feed feed, Long feedViewCount, List<Media> mediaList) {
@@ -31,12 +34,19 @@ public record FeedDetailResDto(
             feed.getContent(),
             feed.getViewCount() + feedViewCount,
             convertToMediaResDto(mediaList),
+            convertToCategoryDto(feed.getCategories()),
             feed.getLastModifiedTime());
     }
 
     private static List<MediaResDto> convertToMediaResDto(List<Media> mediaList){
         return mediaList.stream().map(
                 MediaResDto::fromFeedDetail
+        ).collect(Collectors.toList());
+    }
+
+    private static List<CategoryDto> convertToCategoryDto(List<FeedCategoryMapping> mappings){
+        return mappings.stream().map(
+                m -> new CategoryDto(m.getFirstCategory().getId(), m.getSecondCategory().getId(), m.getThirdCategory().getId())
         ).collect(Collectors.toList());
     }
 }
