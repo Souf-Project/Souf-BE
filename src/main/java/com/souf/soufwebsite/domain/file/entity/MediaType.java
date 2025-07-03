68be91001cffcd6f4b6548031c283da30cf29e9e
wjd4204 <brian876373@gmail.com>
2025-07-03 18:45:59 +0900

    refactor: 미디어 엔티티 enum 타입 변경

--- src/main/java/com/souf/soufwebsite/domain/file/entity/MediaType.java
+++ src/main/java/com/souf/soufwebsite/domain/file/entity/MediaType.java
@@ -2,5 +2,6 @@ package com.souf.soufwebsite.domain.file.entity;
 
 public enum MediaType {
 
-    JPG, JPEG, PNG, PDF, GIF, WEBP, VIDEO
+    JPG, JPEG, PNG, PDF, GIF, WEBP,
+    MP4, MOV, AVI, MKV, WEBM, FLV
 }
