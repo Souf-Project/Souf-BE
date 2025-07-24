7538d3494e88dc29f5918cd153ccfdccf80ac852
wjd4204 <brian876373@gmail.com>
2025-07-24 15:06:37 +0900

    feat: 이벤트 발생 시 슬랙에 메시지 전송 기능 구현

---
+++ src/main/java/com/souf/soufwebsite/global/slack/SlackConstant.java
@@ -0,0 +1,8 @@
+package com.souf.soufwebsite.global.slack;
+
+public class SlackConstant {
+
+    public static final String SIGNUP_CHANNEL = "#스프-회원가입-알림";
+    public static final String POST_CREATE_CHANNEL = "#스프-게시글생성-알림";
+    public static final String ERROR_CHANNEL = "#스프-오류-알림";
+}
