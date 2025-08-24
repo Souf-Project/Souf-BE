cc7e418ec8f91468450697110f70b11b9078e09a
wjd4204 <brian876373@gmail.com>
2025-08-24 17:57:24 +0900

    feat: 신고 기능 exception 추가

--- src/main/java/com/souf/soufwebsite/domain/report/exception/ErrorType.java
+++ src/main/java/com/souf/soufwebsite/domain/report/exception/ErrorType.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorType {
 
-    NOT_MATCHED_REPORT_OWNER(404, "신고자와 현재 사용자가 일치하지 않습니다.");
+    NOT_MATCHED_REPORT_OWNER(404, "신고자와 현재 사용자가 일치하지 않습니다."),
+
+    /* ============================== 신고 사유 ===================================== */
+
+    NOT_FOUND_REPORT_REASON(404, "해당 신고 사유가 존재하지 않습니다.");
 
     private final int code;
     private final String message;
