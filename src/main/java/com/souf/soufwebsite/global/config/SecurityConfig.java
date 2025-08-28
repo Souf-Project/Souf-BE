3ef86786ecb31ad52a518a91e61c9452e3949016
wjd4204 <brian876373@gmail.com>
2025-08-28 14:55:06 +0900

    refactor: 제재당한 사용자 필터링 제외(구현중)

--- src/main/java/com/souf/soufwebsite/global/config/SecurityConfig.java
+++ src/main/java/com/souf/soufwebsite/global/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.souf.soufwebsite.global.config;
 
 import com.souf.soufwebsite.domain.member.repository.MemberRepository;
 import com.souf.soufwebsite.domain.report.service.BanService;
-import com.souf.soufwebsite.global.jwt.BanCheckFilter;
 import com.souf.soufwebsite.global.jwt.JwtAuthenticationFilter;
 import com.souf.soufwebsite.global.jwt.JwtLogoutHandler;
 import com.souf.soufwebsite.global.jwt.JwtServiceImpl;
@@ -69,8 +68,8 @@ public class SecurityConfig {
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(new BanCheckFilter(banService), JwtAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//                .addFilterAfter(new BanCheckFilter(banService), JwtAuthenticationFilter.class);
 
         http
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
