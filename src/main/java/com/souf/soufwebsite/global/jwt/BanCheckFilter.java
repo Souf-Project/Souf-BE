e96d90bbabd4018a4565818c09b168e6cf2ae0aa
wjd4204 <brian876373@gmail.com>
2025-08-29 14:39:53 +0900

    fix: 제재받은 사용자가 로그인 할 수 없도록 로직 수정

--- src/main/java/com/souf/soufwebsite/global/jwt/BanCheckFilter.java
+++ src/main/java/com/souf/soufwebsite/global/jwt/BanCheckFilter.java
@@ -8,12 +8,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 public class BanCheckFilter extends OncePerRequestFilter {
 
