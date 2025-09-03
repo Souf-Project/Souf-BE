46f29428871e6f66e894c76653fe3c68fe9233a5
wjd4204 <brian876373@gmail.com>
2025-09-03 17:02:09 +0900

    refactor: 신고글 관련 API 리팩터링

--- src/main/java/com/souf/soufwebsite/domain/report/controller/ReportController.java
+++ src/main/java/com/souf/soufwebsite/domain/report/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.souf.soufwebsite.domain.report.controller;
 import com.souf.soufwebsite.domain.report.dto.ReportReqDto;
 import com.souf.soufwebsite.domain.report.service.ReportService;
 import com.souf.soufwebsite.global.success.SuccessResponse;
+import com.souf.soufwebsite.global.util.CurrentEmail;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,9 +24,10 @@ public class ReportController implements ReportApiSpecification {
 
     @PostMapping
     public SuccessResponse createReport(
+            @CurrentEmail String email,
             @RequestBody @Valid ReportReqDto reportReqDto
     ) {
-        reportService.createReport(reportReqDto);
+        reportService.createReport(email, reportReqDto);
 
         return new SuccessResponse(REPORT_CREATE.getMessage());
     }
