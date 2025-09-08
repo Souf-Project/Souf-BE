75d0a79838c3d328b657052342903d0569867504
wjd4204 <brian876373@gmail.com>
2025-09-08 17:37:02 +0900

    refactor: 소셜 로그인 관련 exception에 errorKey 필드 추가

--- src/main/java/com/souf/soufwebsite/domain/socialAccount/exception/AlreadyLinkedOtherUserException.java
+++ src/main/java/com/souf/soufwebsite/domain/socialAccount/exception/AlreadyLinkedOtherUserException.java
@@ -2,6 +2,9 @@ package com.souf.soufwebsite.domain.socialAccount.exception;
 
 import com.souf.soufwebsite.global.exception.BaseErrorException;
 
+import static com.souf.soufwebsite.domain.socialAccount.exception.ErrorType.ALREADY_LINKED_OTHER_USER;
+
 public class AlreadyLinkedOtherUserException extends BaseErrorException {
-    public AlreadyLinkedOtherUserException(){super(ErrorType.ALREADY_LINKED_OTHER_USER.getCode(), ErrorType.ALREADY_LINKED_OTHER_USER.getMessage());}
+    public AlreadyLinkedOtherUserException(){
+        super(ALREADY_LINKED_OTHER_USER.getCode(), ALREADY_LINKED_OTHER_USER.getMessage(), ALREADY_LINKED_OTHER_USER.getErrorKey());}
 }
--- src/main/java/com/souf/soufwebsite/domain/socialAccount/exception/NotValidAuthenticationException.java
+++ src/main/java/com/souf/soufwebsite/domain/socialAccount/exception/NotValidAuthenticationException.java
@@ -5,5 +5,6 @@ import com.souf.soufwebsite.global.exception.BaseErrorException;
 import static com.souf.soufwebsite.domain.socialAccount.exception.ErrorType.NOT_VALID_AUTHENTICATION;
 
 public class NotValidAuthenticationException extends BaseErrorException {
-    public NotValidAuthenticationException() {super(NOT_VALID_AUTHENTICATION.getCode(), NOT_VALID_AUTHENTICATION.getMessage());}
+    public NotValidAuthenticationException() {
+        super(NOT_VALID_AUTHENTICATION.getCode(), NOT_VALID_AUTHENTICATION.getMessage(), NOT_VALID_AUTHENTICATION.getErrorKey());}
 }
