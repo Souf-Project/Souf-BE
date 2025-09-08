5758c7ecc03766976ead07c8ac731b4356f7a644
wjd4204 <brian876373@gmail.com>
2025-09-08 16:58:47 +0900

    refactor: 공고문 exception에 errorKey 필드 추가

--- src/main/java/com/souf/soufwebsite/domain/recruit/exception/NotFoundRecruitException.java
+++ src/main/java/com/souf/soufwebsite/domain/recruit/exception/NotFoundRecruitException.java
@@ -2,10 +2,11 @@ package com.souf.soufwebsite.domain.recruit.exception;
 
 import com.souf.soufwebsite.global.exception.BaseErrorException;
 
-import static com.souf.soufwebsite.domain.recruit.exception.ErrorType._NOT_FOUND_RECRUIT;
+import static com.souf.soufwebsite.domain.recruit.exception.ErrorType.NOT_FOUND_RECRUIT;
+
 
 public class NotFoundRecruitException extends BaseErrorException {
     public NotFoundRecruitException() {
-        super(_NOT_FOUND_RECRUIT.getCode(), _NOT_FOUND_RECRUIT.getMessage());
+        super(NOT_FOUND_RECRUIT.getCode(), NOT_FOUND_RECRUIT.getMessage(), NOT_FOUND_RECRUIT.getErrorKey());
     }
 }
