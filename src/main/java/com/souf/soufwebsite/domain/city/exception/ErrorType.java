2f380e4e20ee0210845afde7929f0a7ba4b6425b
tjdgus3488 <tjdgus3488@naver.com>
2025-09-19 16:09:48 +0900

    chore: 신고 errorKey RP로 변경

--- src/main/java/com/souf/soufwebsite/domain/city/exception/ErrorType.java
+++ src/main/java/com/souf/soufwebsite/domain/city/exception/ErrorType.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorType {
 
-    NOT_FOUND_CITY(404, "유효하지 않은 도시입니다.", "C404-1"),
-    NOT_FOUND_CITY_DETAIL(404, "유효하지 않은 지역입니다.", "C404-2"),
-    REQUIRED_CITY_DETAIL(400, "상세지역은 필수입니다.", "C400-1"),;
+    NOT_FOUND_CITY(404, "유효하지 않은 도시입니다.", "CT404-1"),
+    NOT_FOUND_CITY_DETAIL(404, "유효하지 않은 지역입니다.", "CT404-2"),
+    REQUIRED_CITY_DETAIL(400, "상세지역은 필수입니다.", "CT400-1"),;
 
 
     private final int code;
