028273ca696989697855813ccc369f6f6bfbb616
wjd4204 <brian876373@gmail.com>
2025-09-30 19:11:12 +0900

    hot-fix: MediaReqDto에 대한 validation 수정

--- src/main/java/com/souf/soufwebsite/domain/file/dto/MediaReqDto.java
+++ src/main/java/com/souf/soufwebsite/domain/file/dto/MediaReqDto.java
@@ -1,14 +1,13 @@
 package com.souf.soufwebsite.domain.file.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record MediaReqDto(
         @Schema(description = "해당 공고문/피드의 아이디", example = "1")
-        @NotBlank(message = "아이디는 필수입니다.")
+        @NotNull(message = "아이디는 필수입니다.")
         Long postId,
 
         @Schema(description = "해당 파일에 접근할 수 있는 url", example = "https://iamsouf.s3.amazonaws.com/feed/original/example.jpg" )
