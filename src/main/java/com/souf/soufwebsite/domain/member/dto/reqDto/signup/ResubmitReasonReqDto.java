51abb3738ffef8fe6c9e10e72e58c04080362a4b
wjd4204 <brian876373@gmail.com>
2025-11-02 19:16:49 +0900

    fix: 거절시 reqDto의 originalURL을 통한 S3 이미지 삭제 이벤트 추가

--- src/main/java/com/souf/soufwebsite/domain/member/dto/reqDto/signup/ResubmitReasonReqDto.java
+++ src/main/java/com/souf/soufwebsite/domain/member/dto/reqDto/signup/ResubmitReasonReqDto.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotNull;
 
 public record ResubmitReasonReqDto(
 
+        @Schema(description = "거절을 한다면 관리자 페이지 내의 멤버 상세조희의 인증서류 url을 보내주세요.", example = "profile/authentication/origin/...")
+        String originalUrl,
+
         @Schema(description = "재제출을 요구하는 이유를 메일에 표기합니다.")
         @NotNull(message = "재제출 사유를 적어주세요.")
         String reason
