4f8f5f62934e45c756b8954e1fcb8a051bbae144
wjd4204 <brian876373@gmail.com>
2025-11-17 23:45:24 +0900

    fix: 불필요한 enum status 제거

--- src/main/java/com/souf/soufwebsite/domain/member/entity/ApprovedStatus.java
+++ src/main/java/com/souf/soufwebsite/domain/member/entity/ApprovedStatus.java
@@ -9,8 +9,7 @@ public enum ApprovedStatus {
 
     PENDING("승인 대기 중"),
     APPROVED("승인 완료"),
-    REJECTED("승인 거절"),
-    COMPLETED("완료");
+    REJECTED("승인 거절");
 
     private final String status;
 }
