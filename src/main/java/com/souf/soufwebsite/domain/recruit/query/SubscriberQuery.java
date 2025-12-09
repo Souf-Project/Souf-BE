bf4fe0bce3e64bf0736a19577a717fda46f85474
tjdgus3488 <tjdgus3488@naver.com>
2025-12-09 15:44:24 +0900

    feat: 공고문 업로드 알람의 구독자 기준을 대분류로 수정

--- src/main/java/com/souf/soufwebsite/domain/recruit/query/SubscriberQuery.java
+++ src/main/java/com/souf/soufwebsite/domain/recruit/query/SubscriberQuery.java
@@ -3,5 +3,5 @@ package com.souf.soufwebsite.domain.recruit.query;
 import java.util.List;
 
 public interface SubscriberQuery {
-    List<Long> findSubscriberIdsByFirstSecond(Long firstCategoryId, Long secondCategoryId);
+    List<Long> findSubscriberIdsByFirst(Long firstCategoryId);
 }
