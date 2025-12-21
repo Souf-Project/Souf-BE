59964e5bf76cad765ef24ed6b77d25fae4b76e0e
tjdgus3488 <tjdgus3488@naver.com>
2025-12-22 05:07:14 +0900

    fix: tokenResDto 필드 추가 및 JsonIgnoreProperties 사용

--- src/main/java/com/souf/soufwebsite/domain/socialAccount/dto/google/GoogleTokenResDto.java
+++ src/main/java/com/souf/soufwebsite/domain/socialAccount/dto/google/GoogleTokenResDto.java
@@ -1,9 +1,23 @@
 package com.souf.soufwebsite.domain.socialAccount.dto.google;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GoogleTokenResDto(
         @JsonProperty("access_token")
-        String accessToken
-) {
-}
+        String accessToken,
+
+        @JsonProperty("token_type")
+        String tokenType,
+
+        @JsonProperty("expires_in")
+        Long expiresIn,
+
+        @JsonProperty("refresh_token")
+        String refreshToken,
+
+        @JsonProperty("scope")
+        String scope
+
+) {}
