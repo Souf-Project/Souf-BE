d5064037aa94b8cf0ac87a9dc862ea7d2ed21c82
tjdgus3488 <tjdgus3488@naver.com>
2025-12-22 16:42:17 +0900

    userResDto ignoreUnknown 추가 작성

--- src/main/java/com/souf/soufwebsite/domain/socialAccount/dto/google/GoogleUserResDto.java
+++ src/main/java/com/souf/soufwebsite/domain/socialAccount/dto/google/GoogleUserResDto.java
@@ -1,12 +1,20 @@
 package com.souf.soufwebsite.domain.socialAccount.dto.google;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GoogleUserResDto(
         @JsonProperty("sub")
         String id,
+
+        @JsonProperty("name")
         String name,
+
+        @JsonProperty("email")
         String email,
+
+        @JsonProperty("picture")
         String picture
-) {
-}
+) {}
