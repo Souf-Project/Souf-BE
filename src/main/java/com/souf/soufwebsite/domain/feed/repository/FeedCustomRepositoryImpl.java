aa2ec14521e769ea844e3cdef75ecd53a03db2aa
wjd4204 <brian876373@gmail.com>
2026-01-27 18:13:07 +0900

    fix: where 조건을 2월에 생성된 like가 아닌 feed로 변경

--- src/main/java/com/souf/soufwebsite/domain/feed/repository/FeedCustomRepositoryImpl.java
+++ src/main/java/com/souf/soufwebsite/domain/feed/repository/FeedCustomRepositoryImpl.java
@@ -69,8 +69,8 @@ public class FeedCustomRepositoryImpl implements FeedCustomRepository {
                 ).from(feed)
                 .leftJoin(likedFeed).on(
                         feed.id.eq(likedFeed.feedId),
-                        likedFeed.createdTime.goe(s),
-                        likedFeed.createdTime.lt(e))
+                        feed.createdTime.goe(s),
+                        feed.createdTime.lt(e))
                 .where(feed.member.id.eq(mId))
                 .groupBy(feed.id, feed.topic)
                 .orderBy(likeCnt.desc(), feed.id.asc())
