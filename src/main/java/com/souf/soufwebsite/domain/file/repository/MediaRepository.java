8f25c27a31effed0db3adda25205e65d1c24ecaa
tjdgus3488 <tjdgus3488@naver.com>
2026-02-28 23:37:34 +0900

    feat: post에 따른 media 조회 시 in 절 추가

--- src/main/java/com/souf/soufwebsite/domain/file/repository/MediaRepository.java
+++ src/main/java/com/souf/soufwebsite/domain/file/repository/MediaRepository.java
@@ -5,6 +5,7 @@ import com.souf.soufwebsite.global.common.PostType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -16,6 +17,17 @@ public interface MediaRepository extends JpaRepository<Media, Long> {
 
     @Query("select m from Media m where m.postType = :postType and m.postId = :postId")
     List<Media> findByPostTypeAndPostId(PostType postType, Long postId);
+
+    @Query("""
+    SELECT m
+    FROM Media m
+    WHERE m.postType = :postType
+      AND m.postId IN :postIds
+    ORDER BY m.id ASC
+    """)
+    List<Media> findByPostTypeAndPostIdIn(
+            @Param("postType") PostType postType, @Param("postIds") List<Long> postIds
+    );
 
     @Modifying
     @Query("delete from Media m where m.postType = :postType and m.postId = :postId")
