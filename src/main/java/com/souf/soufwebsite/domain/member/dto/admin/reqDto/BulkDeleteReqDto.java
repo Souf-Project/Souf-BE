59a8170a28be5402e6028497561c30540ca6e48f
tjdgus3488 <tjdgus3488@naver.com>
2026-03-03 22:37:13 +0900

    docs: 게시물 bulk 삭제 dto 스웨거 작성

--- src/main/java/com/souf/soufwebsite/domain/member/dto/admin/reqDto/BulkDeleteReqDto.java
+++ src/main/java/com/souf/soufwebsite/domain/member/dto/admin/reqDto/BulkDeleteReqDto.java
@@ -1,8 +1,13 @@
 package com.souf.soufwebsite.domain.member.dto.admin.reqDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record BulkDeleteReqDto(
+        @Schema(description = "삭제할 회원들의 아이디 리스트", example = "[1, 2, 3]")
         List<Long> ids,
+
+        @Schema(description = "삭제 사유", example = "서비스 이용 약관 위반")
         String reason
 ) {}
